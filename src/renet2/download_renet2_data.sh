a640089019bc85421e40380510851f965f1a09ce
Junhao <sujunhao.hit@gmail.com>
2021-03-30 15:34:06 +0800

    Update download_renet2_data.sh

--- src/renet2/download_renet2_data.sh
+++ src/renet2/download_renet2_data.sh
@@ -2,25 +2,5 @@
 set -x
 
 mkdir -p $1
-cd $1
-mkdir -p data/{abs_data,ft_data,ft_info}
-mkdir -p models
 
-# download data
-## download abstract dataset [dir: renet2/data/abs_data]
-(cd data/abs_data && wget http://www.bio8.cs.hku.hk/RENET2/renet2_abs_data.tar.gz && tar -xf renet2_abs_data.tar.gz && rm renet2_abs_data.tar.gz )
-
-## download full-text dataset [dir: renet2/data/ft_data]
-(cd data/ft_data && wget http://www.bio8.cs.hku.hk/RENET2/renet2_ft_data.tar.gz && tar -xf renet2_ft_data.tar.gz && rm renet2_ft_data.tar.gz )
-(cd data/ft_info && wget http://www.bio8.cs.hku.hk/RENET2/renet2_ft_data_info.tar.gz && tar -xf renet2_ft_data_info.tar.gz && rm renet2_ft_data_info.tar.gz )
-
-## download gene-disease assoications found from PMC and LitCovid [dir: renet2/data/pmc_litcovid]
-(cd data && wget http://www.bio8.cs.hku.hk/RENET2/renet2_gda_pmc_litcovid_rst.tar.gz && tar -xf renet2_gda_pmc_litcovid_rst.tar.gz && rm renet2_gda_pmc_litcovid_rst.tar.gz)
-
-# download trained model
-## download trained abstract model [dir: renet2/models/abs_models]
-(cd models && wget http://www.bio8.cs.hku.hk/RENET2/renet2_trained_models_abs.tar.gz && tar -xf renet2_trained_models_abs.tar.gz && rm renet2_trained_models_abs.tar.gz)
-
-## download trained abstract model [dir: renet2/models/ft_models]
-(cd models && wget http://www.bio8.cs.hku.hk/RENET2/renet2_trained_models_ft.tar.gz && tar -xf renet2_trained_models_ft.tar.gz && rm renet2_trained_models_ft.tar.gz)
-
+(cd $1 && wget http://www.bio8.cs.hku.hk/RENET2/renet2_data_models.tar.gz && tar -xf renet2_data_models.tar.gz && rm renet2_data_models.tar.gz )
